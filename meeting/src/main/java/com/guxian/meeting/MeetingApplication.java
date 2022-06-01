2b7d1921fc31fd9bc531ab0bcfb8d643661beed0
GuXian <2818958193@qq.com>
2022-06-01 11:27:58 +0800

    补充MapperScan 可以正常运行

--- meeting/src/main/java/com/guxian/meeting/MeetingApplication.java
+++ meeting/src/main/java/com/guxian/meeting/MeetingApplication.java
@@ -1,8 +1,10 @@
 package com.guxian.meeting;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.guxian.meeting.mapper")
 @SpringBootApplication
 public class MeetingApplication {
 
