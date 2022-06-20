cbcdcff7e6866cf349b5cd4e48f59a4486dea79c
GuXian <2818958193@qq.com>
2022-06-20 20:28:33 +0800

    接口去/

--- meeting/src/main/java/com/guxian/meeting/controller/MeetingController.java
+++ meeting/src/main/java/com/guxian/meeting/controller/MeetingController.java
@@ -50,14 +50,14 @@ public class MeetingController {
      * @return
      */
 
-    @PostMapping("/")
+    @PostMapping
     public ResponseData createMeeting(@RequestBody @Validated(AddGroup.class) MeetingVo meeting) {
         return ResponseData.success()
                 .data(meetingService.addMeeting(meeting.toMeeting())
                         .orElseThrow(() -> new ServiceException(BizCodeEnum.CREATE_MEETING_FAILED)));
     }
 
-    @PatchMapping("/")
+    @PatchMapping
     public ResponseData updateMeeting(@RequestBody @Validated(UpdateGroup.class) MeetingVo meeting) {
         return ResponseData.success()
                 .data(
@@ -79,7 +79,7 @@ public class MeetingController {
                         .setJoinNum((int) userMeetingService.count(new QueryWrapper<UserMeeting>().eq("mid", id))));
     }
 
-    @GetMapping("/")
+    @GetMapping
     public ResponseData getMeetingByName(String name) {
         return ResponseData.success()
                 .data(meetingService.getMeetingByName(name).orElseThrow(() -> new ServiceException(BizCodeEnum.MEETING_NOT_EXIST)));
