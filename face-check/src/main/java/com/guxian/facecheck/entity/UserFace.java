73e0c2773e37a719a100d8d5e52eca77e84eae28
oilmoe <me@oil.moe>
2022-06-22 01:32:52 +0800

    feat:   add UploadService and implement AliOssUploadService . (I don't know if it works)

--- face-check/src/main/java/com/guxian/facecheck/entity/UserFace.java
+++ face-check/src/main/java/com/guxian/facecheck/entity/UserFace.java
@@ -3,7 +3,6 @@ package com.guxian.facecheck.entity;
 import java.io.Serializable;
 import java.util.Date;
 import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.Data;
