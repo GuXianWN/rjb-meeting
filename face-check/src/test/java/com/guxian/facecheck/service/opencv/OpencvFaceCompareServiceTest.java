1483bb52adf56c6e38318e6e8d316c2fc18a55ee
oilmoe <me@oil.moe>
2022-07-02 21:01:39 +0800

    feat: Basic face discrimination is complete

---
+++ face-check/src/test/java/com/guxian/facecheck/service/opencv/OpencvFaceCompareServiceTest.java
@@ -0,0 +1,43 @@
+package com.guxian.facecheck.service.opencv;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.opencv.core.Mat;
+
+import java.io.File;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.within;
+
+class OpencvFaceCompareServiceTest {
+
+    private OpencvFaceCompareService opencvFaceCompareServiceUnderTest;
+
+    @BeforeEach
+    void setUp() {
+        opencvFaceCompareServiceUnderTest = new OpencvFaceCompareService();
+    }
+
+    @Test
+    void testCheckFaceSimilarRate() {
+        // Setup
+        final File faceFileA = new File("C:/Users/32253/Desktop/1.jpg");
+        final File faceFileB = new File("C:/Users/32253/Desktop/2.jpg");
+
+        // Run the test
+        final double result = opencvFaceCompareServiceUnderTest.checkFaceSimilarRate(faceFileA, faceFileB);
+
+        // Verify the results
+        System.out.println(result);
+
+    }
+
+    @Test
+    void testConv_Mat() {
+        // Setup
+        // Run the test
+        final Mat result = opencvFaceCompareServiceUnderTest.conv_Mat("img");
+
+        // Verify the results
+    }
+}
