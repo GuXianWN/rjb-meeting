fc10f98f968fccb0f466eec51a8c5b808a34b9ef
oilmoe <me@oil.moe>
2022-07-06 23:37:35 +0800

    feat: modify User By id

--- auth/src/main/java/com/guxian/auth/service/UserService.java
+++ auth/src/main/java/com/guxian/auth/service/UserService.java
@@ -4,6 +4,7 @@ import com.guxian.auth.entity.User;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.guxian.auth.entity.vo.LoginVo;
 import com.guxian.auth.entity.vo.RegisterVo;
+import com.guxian.common.entity.PageData;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Optional;
@@ -13,7 +14,7 @@ import java.util.Optional;
  */
 public interface UserService extends IService<User> {
 
-    Optional<User> findByUsernameLimitOne(String username);
+    Optional<User> findByAccountLimitOne(String username);
 
     String login(LoginVo loginVo, HttpServletRequest request);
 
@@ -22,4 +23,8 @@ public interface UserService extends IService<User> {
     Optional<User> register(RegisterVo user);
 
     Optional<User> getByUsername(String username);
+
+    void modifyUserById(User user);
+
+    PageData getUserList(Long page, Long size);
 }
