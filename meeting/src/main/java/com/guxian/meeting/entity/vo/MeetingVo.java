ea19c59ae1557feccfad949b964344955f613995
oilmoe <me@oil.moe>
2022-07-14 21:01:50 +0800

    fixed : fixed error message  on validation check

--- meeting/src/main/java/com/guxian/meeting/entity/vo/MeetingVo.java
+++ meeting/src/main/java/com/guxian/meeting/entity/vo/MeetingVo.java
@@ -40,12 +40,11 @@ public class MeetingVo {
     private Date createTime;
 
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
-    @FutureOrPresent(message = "开始时间必须大于当前时间", groups = {AddGroup.class})
+    @FutureOrPresent(message = "开始时间必须大于当前时间并且小于结束时间", groups = {AddGroup.class})
     private Date beginTime;
 
-
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
-    @FutureOrPresent(message = "结束时间必须大于开始时间", groups = {AddGroup.class})
+    @FutureOrPresent(message = "结束时间必须大于开始时间并且大于当前时间", groups = {AddGroup.class})
     private Date endTime;
 
     private MeetingState state;
